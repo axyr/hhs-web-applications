ea3f9bbaa706ffe7855432cdc837b7d88818eb1b
Martijn van Nieuwenhoven <martijn@novat.io>
2023-02-13 00:44:54 +0100

    use real data

--- static/assets/app.js
+++ static/assets/app.js
@@ -1,17 +1,17 @@
 (function () {
 
-    const cards         = [];
-    const amountOfCards = 13;
-    const cardsPerPage  = 5;
-    const categories    = [
-        'Category A',
-        'Category B',
-        'Category C',
-    ];
+    const cardsPerPage = 5;
 
-    let selectedCards = [];
+    const collection = './collections/books/index.json';
 
-    let data = {
+    const data = {
+        items: [],
+        categories: [],
+    };
+
+    let displayedItems = [];
+
+    let settings = {
         currentPage: 1,
         sortDirection: 'asc',
         filterFavorites: false,
@@ -19,49 +19,66 @@
         favoriteCards: [],
     };
 
-    function setData(key, value) {
-        data[key] = value;
-        window.localStorage.setItem('data', JSON.stringify(data));
+    function updateSetting(key, value) {
+        settings[key] = value;
+        window.localStorage.setItem('settings', JSON.stringify(settings));
     }
 
-    function loadDataFromLocalStorage() {
-        const dataFromLocalStorage = window.localStorage.getItem('data');
-        if(dataFromLocalStorage) {
-            const dataObject = JSON.parse(dataFromLocalStorage);
+    function loadSettingsFromLocalStorage() {
+        const settingsFromLocalStorage = window.localStorage.getItem('settings');
+        if (settingsFromLocalStorage) {
+            const settingsObject = JSON.parse(settingsFromLocalStorage);
 
-            for (const [key, value] of Object.entries(dataObject)) {
-                if(data.hasOwnProperty(key)) {
-                    data[key] = value;
+            for (const [key, value] of Object.entries(settingsObject)) {
+                if (settings.hasOwnProperty(key)) {
+                    settings[key] = value;
                 }
             }
         }
     }
 
+    function fetchItems() {
+        fetch(collection).then(response => {
+            return response.json();
+        }).catch(error => {
+            console.log(error);
+            console.log('Could not load items');
+        }).then(json => {
+            data.items      = json.items;
+            data.categories = json.categories;
+
+            data.items.forEach(function (item) {
+                data.categories.forEach(function (category) {
+                    if (item.categoryId === category.id) {
+                        category.itemCount++;
+                    }
+                });
+            });
+
+            redraw();
+        });
+    }
+
     function init() {
-        loadDataFromLocalStorage();
-        createCards();
-        redraw();
+        loadSettingsFromLocalStorage();
+        fetchItems();
 
         document.getElementById('logo').addEventListener('click', toToHomepage);
         document.getElementById('sort').addEventListener('change', handleSortChange);
     }
 
     function setPageTitle() {
-        document.getElementsByTagName('title')[0].text = `Page ${data.currentPage} - Books`;
+        document.getElementsByTagName('title')[0].text = `Page ${settings.currentPage} - Books`;
     }
 
     function toToHomepage() {
-        setData('selectedCategories', []);
+        updateSetting('selectedCategories', []);
         setCurrentPage(1);
     }
 
     function setCurrentPage(page) {
-        setData('currentPage', parseInt(page));
+        updateSetting('currentPage', parseInt(page));
         redraw();
-    }
-
-    function getRandomCategoryId() {
-        return Math.floor(Math.random() * Object.keys(categories).length);
     }
 
     function getClearedElementById(id) {
@@ -71,25 +88,14 @@
         return element;
     }
 
-    function createCards() {
-        for (let i = 1; i <= amountOfCards; i++) {
-            const card = {
-                id: i,
-                title: `Book ${i}`,
-                categoryId: getRandomCategoryId(),
-            };
-            cards.push(card);
-        }
-    }
+    function displayCards() {
 
-    function selectCards() {
+        const low  = settings.sortDirection === 'desc' ? -1 : 1;
+        const high = settings.sortDirection === 'desc' ? 1 : -1;
 
-        const low  = data.sortDirection === 'desc' ? -1 : 1;
-        const high = data.sortDirection === 'desc' ? 1 : -1;
-
-        selectedCards = cards
-            .filter(card => !Object.keys(data.selectedCategories).length || data.selectedCategories.includes(card.categoryId))
-            .filter(card => !data.filterFavorites || data.favoriteCards.includes(card.id))
+        displayedItems = data.items
+            .filter(item => !Object.keys(settings.selectedCategories).length || settings.selectedCategories.includes(item.categoryId))
+            .filter(item => !settings.filterFavorites || settings.favoriteCards.includes(item.id))
             .sort((a, b) => (a.id > b.id) ? low : high);
 
         renderCards();
@@ -98,10 +104,10 @@
     function renderCards() {
         const targetElement = getClearedElementById('cards');
 
-        const firstItem = (data.currentPage * cardsPerPage) - cardsPerPage;
+        const firstItem = (settings.currentPage * cardsPerPage) - cardsPerPage;
         const lastItem  = firstItem + cardsPerPage;
 
-        selectedCards
+        displayedItems
             .slice(firstItem, lastItem)
             .forEach(function (card) {
                 const element         = appendCard(targetElement, card);
@@ -124,13 +130,13 @@
 
     function renderMenuItems() {
         const targetElement     = getClearedElementById('main-nav');
-        const numberOfMenuItems = Math.ceil(Object.keys(selectedCards).length / cardsPerPage);
+        const numberOfMenuItems = Math.ceil(Object.keys(displayedItems).length / cardsPerPage);
 
         for (let i = 1; i <= numberOfMenuItems; i++) {
             const menuItem = {
                 id: i,
                 title: `Page ${i}`,
-                active: i === data.currentPage,
+                active: i === settings.currentPage,
             };
 
             const element = appendMenuItem(targetElement, menuItem);
@@ -148,17 +154,18 @@
 
     function renderCategoryCheckboxes() {
         const targetElement = getClearedElementById('categories');
-        categories.forEach(function (category, index) {
-            const checked = data.selectedCategories.includes(index);
-            const element = appendCheckbox(targetElement, index, category, checked);
+
+        data.categories.forEach(function (category) {
+            const checked = settings.selectedCategories.includes(category.id);
+            const element = appendCheckbox(targetElement, category.id, `${category.title} (${category.itemCount})`, checked);
             element.addEventListener('change', handleCategoryChange);
         });
     }
 
     function renderFavoriteCheckbox() {
         const targetElement = getClearedElementById('favorites');
-        const favoriteCount = Object.keys(data.favoriteCards).length;
-        const element       = appendCheckbox(targetElement, 'favorite', `Favorites (${favoriteCount})`, data.filterFavorites);
+        const favoriteCount = Object.keys(settings.favoriteCards).length;
+        const element       = appendCheckbox(targetElement, 'favorite', `Favorites (${favoriteCount})`, settings.filterFavorites);
         element.addEventListener('change', handleFavoriteChange);
     }
 
@@ -171,18 +178,20 @@
     }
 
     function cardTemplate(card) {
-        const category = categories[card.categoryId];
+        const category      = data.categories.find(category => category.id === card.categoryId);
+        const favoriteImage = settings.favoriteCards.includes(card.id) ? './assets/img/heart-solid.svg' : './assets/img/heart-regular.svg';
 
-        const favoriteImage = data.favoriteCards.includes(card.id) ? './assets/img/heart-solid.svg' : './assets/img/heart-regular.svg';
         return `
-            <img src="https://picsum.photos/300/150" alt="placeholder">
+            <div class="image">
+                <img src="${card.img}" alt="${card.title}">
+            </div>
             <div class="content">
                 <span class="number bg-gray">${card.id}</span>
                 <div class="content-inner">
                     <h3>${card.title}</h3>
                 </div>
                 <div class="category bg-gray">
-                    ${category}
+                    ${category.title}
                     <button class="favorite" title="Like!" value="${card.id}">
                         <img src="${favoriteImage}" alt="Like!">
                     </button>
@@ -214,27 +223,27 @@
             }
         });
 
-        setData('selectedCategories', selectedCategories);
+        updateSetting('selectedCategories', selectedCategories);
 
         setCurrentPage(1);
         redraw();
     }
 
     function handleFavoriteChange(e) {
-        setData('filterFavorites', e.target.checked);
+        updateSetting('filterFavorites', e.target.checked);
         setCurrentPage(1);
         redraw();
     }
 
     function handleSortChange() {
-        setData('sortDirection', document.getElementById('sort').value);
+        updateSetting('sortDirection', document.getElementById('sort').value);
         setCurrentPage(1);
         redraw();
     }
 
     function toggleFavorite(id) {
-        let favoriteCards = data.favoriteCards;
-        id = parseInt(id);
+        let favoriteCards = settings.favoriteCards;
+        id                = parseInt(id);
 
         if (favoriteCards.includes(id)) {
             favoriteCards = favoriteCards.filter(e => e !== id)
@@ -242,7 +251,7 @@
             favoriteCards.push(id);
         }
 
-        setData('favoriteCards', favoriteCards);
+        updateSetting('favoriteCards', favoriteCards);
 
         redraw();
     }
@@ -250,7 +259,7 @@
     function redraw() {
         renderCategoryCheckboxes();
         renderFavoriteCheckbox();
-        selectCards();
+        displayCards();
         setPageTitle();
         renderMenuItems();
     }
