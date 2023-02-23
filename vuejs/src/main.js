8f1eca7bdc357cabdf4946f471f3f5318b8e9a44
Martijn van Nieuwenhoven <martijn@novat.io>
2023-02-23 17:09:23 +0100

    fix route fallback

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,34 +1,34 @@
-import { createApp } from "vue/dist/vue.esm-bundler";
-import { createRouter, createWebHistory } from "vue-router";
-import { createPinia } from "pinia";
+import {createApp} from "vue/dist/vue.esm-bundler";
+import {createRouter, createWebHistory} from "vue-router";
+import {createPinia} from "pinia";
 
 import App from "./App.vue";
 
 import "./assets/app.css";
 
 const routes = [
-  {
-    name: "home",
-    path: "/",
-    component: App,
-  },
-  {
-    name: "collections",
-    path: "/collections/:collection",
-    component: App,
-  },
-  {
-    path: "/:catchAll(.*)",
-    redirect: () => {
-      return { path: "/" };
+    {
+        name: "home",
+        path: "/",
+        component: App,
     },
-  },
+    {
+        name: "collections",
+        path: "/collection/:collection",
+        component: App,
+    },
+    {
+        path: "/:catchAll(.*)",
+        redirect: () => {
+            return {path: "/"};
+        },
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  base: window.location.origin,
-  routes,
+    history: createWebHistory(),
+    base: window.location.origin,
+    routes,
 });
 
 const pinia = createPinia();
