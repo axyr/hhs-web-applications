cb540ccb44e42f60a91a7b0562d60495f3d6d295
Martijn van Nieuwenhoven <martijn@novat.io>
2023-03-19 19:07:17 +0100

    Support api versioning

--- backend-node/app/routes/api.js
+++ backend-node/app/routes/api.js
@@ -2,29 +2,29 @@ const collections = require('../controllers/CollectionController');
 const categories = require('../controllers/CategoryController');
 const items = require('../controllers/ItemController');
 
-const router = {
-    init(app) {
-        app.get('/', (req, res) => res.json({title: 'Collection Manager'}));
+const routes = {
+    init(router) {
+        router.get('/', (req, res) => res.json({title: 'Collection Manager'}));
 
-        app.get('/collections', collections.index);
-        app.post('/collections', collections.create);
-        app.get('/collections/:id', collections.show);
-        app.patch('/collections/:id', collections.update);
-        app.delete('/collections/:id', collections.destroy);
+        router.get('/collections', collections.index);
+        router.post('/collections', collections.create);
+        router.get('/collections/:id', collections.show);
+        router.patch('/collections/:id', collections.update);
+        router.delete('/collections/:id', collections.destroy);
 
-        app.get('/collections/:collectionId/categories', categories.index);
-        app.get('/collections/:collectionId/items', items.index);
+        router.get('/collections/:collectionId/categories', categories.index);
+        router.get('/collections/:collectionId/items', items.index);
 
-        app.post('/categories', categories.create);
-        app.get('/categories/:id', categories.show);
-        app.patch('/categories/:id', categories.update);
-        app.delete('/categories/:id', categories.destroy);
+        router.post('/categories', categories.create);
+        router.get('/categories/:id', categories.show);
+        router.patch('/categories/:id', categories.update);
+        router.delete('/categories/:id', categories.destroy);
 
-        app.post('/items', items.create);
-        app.get('/items/:id', items.show);
-        app.patch('/items/:id', items.update);
-        app.delete('/items/:id', items.destroy);
+        router.post('/items', items.create);
+        router.get('/items/:id', items.show);
+        router.patch('/items/:id', items.update);
+        router.delete('/items/:id', items.destroy);
     }
 };
 
-module.exports = router;
+module.exports = routes;
--- backend-node/bootstrap/app.js
+++ backend-node/bootstrap/app.js
@@ -2,20 +2,28 @@ require('./../framework/env.js');
 
 const express = require('express');
 const cors = require('cors');
-const router = require('./../app/routes/api.js');
-
+const routes = require('./../app/routes/api.js');
+const router = express.Router();
 const app = express();
 
+const routePrefix = '/api/v1';
+
+// Allow everyone to query the app
 app.use(cors());
 
+// Return json responses
 app.use(express.json());
 
 // Don't leak information about our system to the big bad world.
 app.disable('x-powered-by');
 
 // Load all routes
-router.init(app);
+routes.init(router);
 
+// Set api versioning
+app.use(routePrefix, router);
+
+// Start the app with a defined port
 app.start = (port) => {
     port = port || 3000;
 
